78c9d45c661e77cfdd6143a688ff8519044c76c1
Nataliya R <n.rudenko@gmail.com>
2019-09-08 16:39:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function calculatesFarePrice(start, destination) {
   if (distance <= 400) {
     return 0;
   } else if (distance > 400 && distance <= 2000) {
-    return #distanceTravelledInFeet(start, destination) * 0.02;
+    return 2;
   } else if (distance > 2000 && distance <= 2500) {
     return 25;
   } else {
